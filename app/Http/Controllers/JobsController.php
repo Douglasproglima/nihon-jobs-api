f7d4b9a50e15531c9ce33ad51b6081b51c7901e8
douglasproglima <douglasproglima@gmail.com>
2019-08-05 23:13:19 +0900

    Primeiro endpoint JobsAll()

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Job;
 use Illuminate\Http\Request;
 
 class JobsController extends Controller
 {
-    //
+	public function index()
+	{
+		$jobs = Job::with('company')->get();
+		return response()->json($jobs);
+	}
 }
